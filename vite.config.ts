e20712de89cab4fca61a6bb21dd8c75440758eb7
shinichi yokota <yokota101010@gmail.com>
2026-07-18 10:23:09 +0900

    feat: use vite-plugin-singlefile to build standalone double-clickable html

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: './',
   test: {
     globals: true,
